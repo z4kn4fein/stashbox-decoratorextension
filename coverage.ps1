63dd8ed6a30c34baef1ec51394ac4fffe3e7e8c3
Peter Csajtai <peter.csajtai@outlook.com>
2016-01-24 02:16:38 +0100

    Fixed coverage.ps1

--- coverage.ps1
+++ coverage.ps1
@@ -1,6 +1,6 @@
 ﻿$openCoverPath = Join-Path $PSScriptRoot "src\packages\OpenCover.4.6.166\tools\OpenCover.Console.exe"
 $coverallsPath = Join-Path $PSScriptRoot "src\packages\coveralls.io.1.3.4\tools\coveralls.net.exe"
-$reportGeneratorPath = Join-Path $PSScriptRoot "src\packages\ReportGenerator.2.3.5.0\tools\ReportGenerator.exe"
+$reportGeneratorPath = Join-Path $PSScriptRoot "src\packages\ReportGenerator.2.4.0.0\tools\ReportGenerator.exe"
 $testDllPath = Join-Path $PSScriptRoot "src\stashbox.decoratorextension.tests\bin\release\Stashbox.DecoratorExtension.Tests.dll"
 $vsTestPath = "c:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
 $coverageReportDir = Join-Path $PSScriptRoot "coverageresults"
